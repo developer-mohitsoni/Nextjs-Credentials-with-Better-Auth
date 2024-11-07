bc793ddd21b57c07c9097ad4a1725682e02c0bc6
Mohit Soni <developer_mohitsoni@outlook.com>
2024-11-07 20:53:45 +0530

    Implement session validation and display user information on DashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,27 @@
-const DashboardPage = () => {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return redirect("/");
+  }
+
+  const user = session?.user;
   return (
     <>
       <div className="mt-10 text-center">
         <h1 className="text-2xl font-bold underline">
           Welcome to the dashboard
         </h1>
+        <ul>
+          <li>Name: {user.name}</li>
+          <li>Email: {user.email}</li>
+        </ul>
       </div>
     </>
   );
