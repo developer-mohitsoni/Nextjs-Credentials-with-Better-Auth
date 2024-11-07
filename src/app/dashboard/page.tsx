40931aa62f91d9f0f66b82271fbdb25ed4a1ad53
Mohit Soni <developer_mohitsoni@outlook.com>
2024-11-07 20:49:32 +0530

    Add DashboardPage component with welcome message

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+const DashboardPage = () => {
+  return (
+    <>
+      <div className="mt-10 text-center">
+        <h1 className="text-2xl font-bold underline">
+          Welcome to the dashboard
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default DashboardPage;
